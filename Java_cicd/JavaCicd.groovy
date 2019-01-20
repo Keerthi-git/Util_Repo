57fda11b9c056338d4e6dd015ea93ffe82979d96
Keerthi-git <45143108+Keerthi-git@users.noreply.github.com>
2019-01-20 12:58:20 +0530

    Update JavaCicd.groovy

--- Java_cicd/JavaCicd.groovy
+++ Java_cicd/JavaCicd.groovy
@@ -1,1 +1,9 @@
-
+def execute () {
+  stage ('checkout') {
+        git url:prop.URL
+            branch:prop.BRANCH
+            pom=readMavenPom file:prop.POM_FILE
+            artifactId=pom.artifactId
+            version=pom.version
+            print 'checkout success'
+  }
