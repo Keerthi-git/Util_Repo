370daeb473a5c6eaac60fd4f7b3415cc00093e25
Keerthi-git <45143108+Keerthi-git@users.noreply.github.com>
2019-01-20 14:01:29 +0530

    Update Common.groovy

--- common/Common.groovy
+++ common/Common.groovy
@@ -1,1 +1,21 @@
+def uploadArtifactory() {
+  script {
+    server= Artifactory.server prop.ARTIFACTORY_ID
+            uploadSpec="""{
+            "files":[{
+                "pattern":"target/*.war",
+                "target":"example-repo-local/${artifactId}/${version}.${buildNo}/"
+                }]
+                }"""
+                server.upload(uploadSpec)
+  }
+}
 
+def cleanWorkspace() {
+  script {
+    sh 'rm -rf ../'+jobName+'/*'
+  }
+  print 'cleaned workspace'
+}
+
+return this
